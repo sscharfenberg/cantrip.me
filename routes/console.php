fe06c2e23b1f71675cfd9ccec9e1290e0af06ac1
Sven Scharfenberg <5430451+sscharfenberg@users.noreply.github.com>
2026-05-08 21:10:30 +0200

    execute CleanUpTempUploads even in maintenance mode.

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new CleanupTempUploads)->daily();
+Schedule::job(new CleanupTempUploads)->daily()->evenInMaintenanceMode();
